208dc21f538c165201de5339a6bd5caabd82404a
Jan Pipek <jan.pipek@gmail.com>
2024-09-21 21:45:07 +0200

    speed exported

--- src/py42195/__init__.py
+++ src/py42195/__init__.py
@@ -1,13 +1,24 @@
 from py42195.config import IMPERIAL, METRIC, get_unit_system, set_unit_system
-from py42195.types import Distance, Duration, Pace, distance, duration, pace
+from py42195.types import (
+    Distance,
+    Duration,
+    Pace,
+    distance,
+    duration,
+    pace,
+    Speed,
+    speed,
+)
 
 __all__ = [
     "Distance",
     "Duration",
     "Pace",
+    "Speed",
     "distance",
     "duration",
     "pace",
+    "speed",
     "set_unit_system",
     "get_unit_system",
     "METRIC",
